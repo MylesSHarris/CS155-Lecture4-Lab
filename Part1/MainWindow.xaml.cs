e638782c86b02004ad5ba0a298ad7100a895b0ad
MylesSHarris <69069959+MylesSHarris@users.noreply.github.com>
2020-10-06 09:19:12 -0700

    commit

---
+++ Part1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Part1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int speed = Int32.Parse(speedText.Text);
+            int speedLimit = Int32.Parse(speedLimitText.Text);
+            
+            if (speed <= speedLimit)
+            {
+                statusText.Text = "Legal speed";
+                statusCanvas.Background = Brushes.Green;
+            }
+            else if (speed <= speedLimit + 5)
+            {
+                statusText.Text = "Speeding no penalty";
+                statusCanvas.Background = Brushes.Yellow;
+            }
+            else
+            {
+                statusText.Text = "Speeding penalty";
+                statusCanvas.Background = Brushes.Red;
+            }
+        }
+    }
+}
